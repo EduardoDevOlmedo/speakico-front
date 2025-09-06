d0c96e4d26015476c3e549396896d871ccab3737
EduardoDevOlmedo <eduardolmedi00@gmail.com>
2025-09-06 01:53:38 -0600

    fix: middleware remove for prod TSP

--- middleware.ts
+++ middleware.ts
@@ -1,40 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { jwtVerify } from "jose";
-
-
-function getCookie(req: NextRequest, name: string) {
-  const cookieHeader = req.headers.get("cookie"); 
-  if (!cookieHeader) return null;
-
-  const cookies = cookieHeader.split("; ").reduce<Record<string, string>>((acc, c) => {
-    const [key, ...v] = c.split("=");
-    acc[key] = v.join("="); 
-    return acc;
-  }, {});
-
-  return cookies[name] || null;
-}
-
-
 
 export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-  if (pathname === "/") return NextResponse.next();
-
-  let token = getCookie(req, "token");
-
-  const secretString = process.env.NEXT_PUBLIC_JWT_SECRET;
-  if (!token || !secretString) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-
-  try {
-    const secret = new TextEncoder().encode(secretString);
-    await jwtVerify(token, secret);
-  } catch (error) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
   return NextResponse.next();
 }
 
