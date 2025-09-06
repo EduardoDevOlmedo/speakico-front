a68e1583dacc92a63ac0934ef8c0ca5b4d1f7feb
EduardoDevOlmedo <eduardolmedi00@gmail.com>
2025-09-06 01:25:08 -0600

    fix: middleware update

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,14 @@
-// middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
 
-export async function middleware(req: NextRequest, res: NextResponse) {
+export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-  if (pathname === "/") {
-    return NextResponse.next();
-  }
+  if (pathname === "/") return NextResponse.next();
 
-  const token = req.cookies.get("token")?.value;
+  const authHeader = req.headers.get("authorization");
+  const token = authHeader?.split(" ")[1];
+
   const secretString = process.env.NEXT_PUBLIC_JWT_SECRET;
   if (!token || !secretString) {
     return NextResponse.redirect(new URL("/", req.url));
@@ -29,6 +28,7 @@ export const config = {
     "/chat/:path*",
     "/practice/:path*",
     "/cards/:path*",
+    "/dashboard/:path*",
     "/",
   ],
 };
